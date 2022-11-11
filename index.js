08b8d8196c316e95316221f83b7c352b61077edf
callum <callumpholt@gmail.com>
2022-11-11 15:21:47 +1100

    finished, passed all tests

--- index.js
+++ index.js
@@ -13,3 +13,46 @@ function destructivelyPrependCat(name) {
 function destructivelyRemoveLastCat() {
     cats.pop();
 }
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+
+//INCOMPLETE
+
+function appendCat(name) {
+       const appendCat = [...cats, "Broom"];
+       return appendCat;
+ }
+
+
+//INCOMPLETE
+
+//This function below should create a new array with "arnold" as index=0, then ...cats
+
+function prependCat(name) {
+    const prependCat = ["Arnold",...cats];
+    return prependCat;
+}
+
+//INCOMPLETE
+
+function removeLastCat() {
+    const removeLastCat = cats.slice();
+    removeLastCat.pop();
+    return removeLastCat;
+    
+}
+
+function removeFirstCat() {
+    const removeFirstCat = cats.slice();
+    removeFirstCat.shift();
+    return removeFirstCat;
+}
+
+// function removeFirstCat() {
+
+// }
+
+
