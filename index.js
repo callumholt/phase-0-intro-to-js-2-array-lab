2da26bc75e23a8145b19891ebd9f7581f939f205
callum <callumpholt@gmail.com>
2022-11-11 11:42:48 +1100

    added functions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution here!
+
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
